2513c09928bd92b66565e6ba3ab21baada6116c6
niieeer <clement0201@icloud.com>
2022-04-06 20:23:35 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <nav class="wrapper">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+.wrapper {
+  display: flex;
+  gap: 20px;
+}
+</style>
